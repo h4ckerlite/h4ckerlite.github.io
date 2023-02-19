6416e36498b630a3a49f11e946a18f13281dbf88
H4ckerLite <h4ckerlite@localhost.localdomain>
2023-02-19 14:20:14 -0400

    Nuevo Post...

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230204.211045';
+const cacheName = 'chirpy-20230219.191855';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
