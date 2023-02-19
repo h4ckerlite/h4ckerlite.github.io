9bfc9d6d0994526ada41c4f454a8adf895a2a6d8
H4ckerLite <h4ckerlite@localhost.localdomain>
2023-02-19 14:38:14 -0400

    Actualización

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230219.192943';
+const cacheName = 'chirpy-20230219.193655';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
